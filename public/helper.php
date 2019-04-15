da2f07cda215815afcda097632922c9f07c00802
vincent <phpvincent@163.com>
2019-04-15 18:45:52 +0800

    fix

---
+++ public/helper.php
@@ -0,0 +1,79 @@
+<?php
+	if (!function_exists("route_on_start")) {
+	 	function route_on_start($connection)
+	    {
+	    	//初始化附带信息
+	    	global $redis;
+	    	$redis=new \Redis(); 
+	    	$redis->pconnect('13.250.109.37',6379,1);
+	    	var_dump($redis);
+	    }
+	}
+	if (!function_exists("route_on_connect")) {
+	 	function route_on_connect($connection)
+	    {
+	    	//初始化附带信息
+	    	$con_msg=route_msg_start();
+	        $ip=$connection->getRemoteIp();
+	        global $route_connections;
+	        $connection->msg=['ip'=>$ip];
+	        $route_connections[$ip]=$connection;
+	        echo 'ip:'.$ip;
+	    }
+	}
+    if (!function_exists("route_on_message")) {
+	 	function route_on_message($connection,$data)
+	    {	$data=json_decode($data,true);
+
+	    	if(!isset($data['route'])||!isset($data['ip_info'])){
+	    		$connection->send(ws_return('route or ip_info not found',1));
+	    		return;
+	    	}
+	        $route=$data['route'];
+	        $connection->msg['route']=$route;
+	        if($redis->hGet('routes',$route)==null){
+	        	$redis->hSet('routes',$route,1);
+	        	$redis->hSet('routes_ips',$route,$connection->msg['ip']);
+	        }else{
+	        	$ips=explode(',', $redis->hGet('routes_ips',$route));
+	        	if(!in_array($connection->msg['ip'], $ips)){
+	        		$ips[]=$connection->msg['ip'];
+	        		$redis->hSet('routes',$route,$redis->hGet('routes',$route)+1);
+	        		$redis->hSet('routes_ips',$route,json_encode($ips));
+	        	}	        	
+	        }
+	        $ip_info=$data['ip_info'];
+	        $redis->hSet('route_ip_msg',$connection->msg['ip'],$ip_info);
+	        $connection->send( ws_return('connect_success',0));
+	        return;
+	    }
+	}
+	if (!function_exists("route_on_close")) {
+	 	function route_on_close($connection)
+	    {
+	    	$route_msg=$connection->msg;
+	    	$redis->hSet('routes',$route_msg['route'],$redis->hGet('routes',$route_msg['route'])-1);
+	    	$ips=explode(',', $redis->hGet('routes_ips',$route_msg['route']));
+	    	unset($ips[array_search($route_msg['ip'],$ips)]);
+	    	$redis->hSet('routes_ips',$route_msg['route'],$ips);
+	    	$redis->hDel('route_ip_msg',$route_msg['ip']);
+	    	echo 'del'.json_encode($route_msg);
+	    }
+	}
+	/**
+	 * websocket协议下的信息返回
+	 * @msg 返回信息描述
+	 * @status 返回状态码（0为成功）
+	 */
+	if (!function_exists("ws_return")) {
+	 	function ws_return($msg,$status=0)
+	    {
+	    	return json_encode(['msg'=>$msg,'status'=>$status]);
+	    }
+	}
+	if (!function_exists("route_msg_start")) {
+	 	function route_msg_start()
+	    {
+	    	return ['ip'=>'','route'=>''];
+	    }
+	}
--- public/index.php
+++ public/index.php
@@ -1,83 +1,21 @@
 <?php
 use Workerman\Worker;
 require_once './Workerman/Autoloader.php';
+require_once './helper.php';
+
+$redis=new \Redis;
+var_dump($redis);die;
     // 初始化一个worker容器，监听1234端口
     global $worker;
     Worker::$stdoutFile = '/tmp/'.date('Y_m_d').'stdout.log';
-    $worker = new Worker('text://0.0.0.0:2349');
+    $route_worker = new Worker('websocket://0.0.0.0:2349');
     // 这里进程数必须设置为1
-    $worker->count = 1;
-    $workers=[];
+    $route_worker->count = 1;
+    $route_connections=[];
+   
     // worker进程启动后建立一个内部通讯端口
-    $worker->onWorkerStart = function($worker)
-    {
-        global $workers;
-        $workers[1]=$innet_woker1=new Worker('websocket://0.0.0.0:2350');
-        $innet_woker1->reusePort=true;
-        $innet_woker1->onMessage ='on_message';
-        
-        
-        $innet_woker1->listen();
-        $workers[2]=$innet_woker2=new Worker('websocket://0.0.0.0:2350');
-         $innet_woker2->reusePort=true;
-        $innet_woker2->onMessage='on_message';
-        
-        
-        $innet_woker2->listen();
-        $workers[3]=$innet_woker3=new Worker('websocket://0.0.0.0:2350');
-         $innet_woker3->reusePort=true;
-        $innet_woker3->onMessage='on_message';
-        
-        
-        $innet_woker3->listen();
-    };
-    // 新增加一个属性，用来保存uid到connection的映射
-    $worker->uidConnections = array();
-    // 当有客户端发来消息时执行的回调函数
-    $worker->onMessage = function($connection, $data)use($worker,$wokers)
-    {
-        foreach($wokers as $k => $v){
-        foreach($v->connections as $key => $val){
-                $val->send($data);
-            }
-        }
-    };
-    
-    // 当有客户端连接断开时
-    $worker->onClose = function($connection)use($worker)
-    {
-        
-    };
-function on_message($connection, $data)use($wokers)
-{   foreach($wokers as $k => $v){
-        foreach($v->connections as $key => $val){
-            $val->send($data);
-        }
-    }
-    $connection->send("resend\n");
-}
-    // 向所有验证的用户推送数据
-    function broadcast($message)
-    {
-       global $worker;
-       foreach($worker->uidConnections as $connection)
-       {
-            $connection->send($message);
-       }
-    }
-    
-    // 针对uid推送数据
-    function sendMessageByUid($uid, $message)
-    {
-        global $worker;
-        if(isset($worker->uidConnections[$uid]))
-        {
-            $connection = $worker->uidConnections[$uid];
-            $connection->send($message);
-            return true;
-        }
-        return false;
-    }
-    
+    $route_worker->onWorkerStart='route_on_start';
+    $route_worker->onConnect='route_on_connect';
+    $route_worker->onMessage='route_on_message';
     // 运行所有的worker（其实当前只定义了一个）
     Worker::runAll();
