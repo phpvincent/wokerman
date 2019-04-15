18d61db258cb653ad61653f496d5ecdcfbcef73f
zhang <18238205232@163.com>
2019-04-15 17:32:40 +0800

    cui 04-15

--- public/redis/index.php
+++ public/redis/index.php
@@ -2,7 +2,7 @@
 namespace redis;
 //var_dump(__DIR__."\Base.php");
 //use redis\Base;
-require_once __DIR__."\Base.php";
+require __DIR__."/Base.php";
 //$my_redis = new Base();
 $my_redis = Base::getRedis();
 $aa = $my_redis->set('aa','1234');
