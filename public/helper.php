0f8a1f3da169e130d216f2a0f0343e096e760372
vincent <phpvincent@163.com>
2019-04-24 15:18:09 +0800

    fix

--- public/helper.php
+++ public/helper.php
@@ -47,8 +47,23 @@
 	    		return;
 	    	}
 	    	if(!isset($data['route'])||!isset($data['ip_info'])){
-	    		$connection->send(ws_return('route or ip_info not found',1));
-	    		return;
+	    		if(isset($data['ip_msg'])){
+	    			//用户联系方式存储
+	    			$ip_info=$redis->hGet('route_ip_msg',$connection->msg['ip']);
+	    			if($ip_info==false){
+	    				$connection->send(ws_return('ip_info not access',1));
+	    			    return;
+	    			}else{
+	    				$ip_info=json_decode($ip_info,true);
+	    				$ip_info['ip_msg']=$data['ip_msg'];
+	    				$redis->hSet('route_ip_msg',$connection->msg['ip'],json_encode($ip_info));
+	    				$connection->send(ws_return('ip_msg save success',0));
+	    			    return;
+	    			}
+	    		}else{
+	    			$connection->send(ws_return('route or ip_info not found',1));
+	    			return;
+	    		}
 	    	}
 	        $route=$data['route'];
 	        $connection->msg['route']=$route;
